3d7e1bb15fab8a1a22b47e8ba2e15a24cb52de7b
70n1 <el.70n1@gmail.com>
2016-09-28 19:17:43 +0200

    menú

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.asteroides;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,11 +33,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void lanzarSalir(View view) {
-        finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true; /** true -> el menú ya está visible */}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.acercaDe) {
+            lanzarAcercaDe(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
+    private void lanzarSalir (View view){
+        finish();
+    }
     public void lanzarAcercaDe(View view) {
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
