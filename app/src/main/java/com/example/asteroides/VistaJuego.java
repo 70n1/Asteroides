90d581f0e5857016d0e7487c2e83e57697aee1fb
70n1 <el.70n1@gmail.com>
2016-10-09 19:29:09 +0200

    Creación de clases grafico, juego, vistjuego graficos del juego graficos vectoriales nave  y asteroides

--- app/src/main/java/com/example/asteroides/VistaJuego.java
+++ app/src/main/java/com/example/asteroides/VistaJuego.java
@@ -1,8 +1,15 @@
 package com.example.asteroides;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.PathShape;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -18,10 +25,63 @@ public class VistaJuego extends View {
     private int numAsteroides = 5; // Número inicial de asteroides
     private int numFragmentos = 3; // Fragmentos en que se divide public
 
+    // //// NAVE //////
+    private Grafico nave; // Gráfico de la nave
+    private int giroNave; // Incremento de dirección
+    private double aceleracionNave; // aumento de velocidad
+    private static final int MAX_VELOCIDAD_NAVE = 20; // Incremento estándar de giro y aceleración
+    private static final int PASO_GIRO_NAVE = 5;
+    private static final float PASO_ACELERACION_NAVE = 0.5f;
+
     VistaJuego(Context context, AttributeSet attrs) {
         super(context, attrs);
         Drawable drawableNave, drawableAsteroide, drawableMisil;
-        drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+
+
+        //drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        if (pref.getString("graficos", "1").equals("0")) {
+
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            Path pathNave = new Path();
+            pathNave.moveTo((float) 0, (float) 0);
+            pathNave.lineTo((float) 1, (float) 0.5);
+            pathNave.lineTo((float) 0, (float) 1);
+            pathNave.lineTo((float) 0, (float) 0);
+            ShapeDrawable dNave = new ShapeDrawable(new PathShape(pathNave, 1, 1));
+            dNave.getPaint().setColor(Color.WHITE);
+            dNave.getPaint().setStyle(Paint.Style.STROKE);
+            dNave.setIntrinsicWidth(20);
+            dNave.setIntrinsicHeight(15);
+            drawableNave = dNave;
+
+            Path pathAsteroide = new Path();
+            pathAsteroide.moveTo((float) 0.3, (float) 0.0);
+            pathAsteroide.lineTo((float) 0.6, (float) 0.0);
+            pathAsteroide.lineTo((float) 0.6, (float) 0.3);
+            pathAsteroide.lineTo((float) 0.8, (float) 0.2);
+            pathAsteroide.lineTo((float) 1.0, (float) 0.4);
+            pathAsteroide.lineTo((float) 0.8, (float) 0.6);
+            pathAsteroide.lineTo((float) 0.9, (float) 0.9);
+            pathAsteroide.lineTo((float) 0.8, (float) 1.0);
+            pathAsteroide.lineTo((float) 0.4, (float) 1.0);
+            pathAsteroide.lineTo((float) 0.0, (float) 0.6);
+            pathAsteroide.lineTo((float) 0.0, (float) 0.2);
+            pathAsteroide.lineTo((float) 0.3, (float) 0.0);
+            ShapeDrawable dAsteroide = new ShapeDrawable(new PathShape(pathAsteroide, 1, 1));
+            dAsteroide.getPaint().setColor(Color.WHITE);
+            dAsteroide.getPaint().setStyle(Paint.Style.STROKE);
+            dAsteroide.setIntrinsicWidth(50);
+            dAsteroide.setIntrinsicHeight(50);
+            drawableAsteroide = dAsteroide;
+            setBackgroundColor(Color.BLACK);
+        } else {
+            setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            drawableNave = context.getResources().getDrawable(R.drawable.nave);
+            drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+        }
+
+        nave = new Grafico(this, drawableNave);
         asteroides = new Vector<Grafico>();
         for (int i = 0; i < numAsteroides; i++) {
             Grafico asteroide = new Grafico(this, drawableAsteroide);
@@ -36,9 +96,13 @@ public class VistaJuego extends View {
     @Override
     protected void onSizeChanged(int ancho, int alto, int ancho_anter, int alto_anter) {
         super.onSizeChanged(ancho, alto, ancho_anter, alto_anter); // Una vez que conocemos nuestro ancho y alto.
+        nave.setCenX(ancho / 2);
+        nave.setCenY(alto / 2);
         for (Grafico asteroide : asteroides) {
-            asteroide.setCenX((int) (Math.random() * ancho));
-            asteroide.setCenY((int) (Math.random() * alto));
+            do {
+                asteroide.setCenX((int) (Math.random() * ancho));
+                asteroide.setCenY((int) (Math.random() * alto));
+            } while (asteroide.distancia(nave) < (ancho + alto) / 5);
         }
     }
 
@@ -48,5 +112,6 @@ public class VistaJuego extends View {
         for (Grafico asteroide : asteroides) {
             asteroide.dibujaGrafico(canvas);
         }
+        nave.dibujaGrafico(canvas);
     }
 }
